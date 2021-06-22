0a6985aec28629049d40c2f447549f7fcbb45f9c
Ed89 <eduard89@mail.ru>
2021-06-22 13:51:07 +0300

    sql1

--- zp_kursas.sql
+++ zp_kursas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2021 at 12:23 PM
+-- Generation Time: Jun 22, 2021 at 12:49 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `zp_kursas` (
+  `id` int(11) NOT NULL,
   `Kurso_autorius` varchar(255) NOT NULL,
   `Kurso_ikelimo_data` date NOT NULL,
   `Kurso_pavadinimas` varchar(255) NOT NULL,
@@ -44,9 +45,21 @@ CREATE TABLE `zp_kursas` (
 -- Dumping data for table `zp_kursas`
 --
 
-INSERT INTO `zp_kursas` (`Kurso_autorius`, `Kurso_ikelimo_data`, `Kurso_pavadinimas`, `Kurso_trumpas_aprasas`, `Kurso_platsnis_aprasas`, `Nuotraukos`, `Kurso_populiarumas`, `Kurso_komentarai`, `Kurso_kaina`, `Kurso_reitingas`) VALUES
-('Eduardas Brazauskas', '2021-06-22', 'Web programavimas', 'web programaviams', 'mokesi alia kurti web svetainius', NULL, NULL, NULL, NULL, NULL),
-('Eduardas Brazauskas', '2021-06-22', 'Web programavimas1', 'web programavimas1', 'mokesi alia kurti web svetainius-test', NULL, NULL, 'ok', 500, 4);
+INSERT INTO `zp_kursas` (`id`, `Kurso_autorius`, `Kurso_ikelimo_data`, `Kurso_pavadinimas`, `Kurso_trumpas_aprasas`, `Kurso_platsnis_aprasas`, `Nuotraukos`, `Kurso_populiarumas`, `Kurso_komentarai`, `Kurso_kaina`, `Kurso_reitingas`) VALUES
+(1, 'Autorius 1', '2021-06-22', 'PHP Pagrindai', 'web programaviams', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\"s standard dummy text ever since the 1500s......', 'php.jpg', NULL, NULL, NULL, NULL),
+(2, 'Autorius 2', '2021-06-22', 'HTML/CSS Ivadas', 'web programaviams', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\"s standard dummy text ever since the 1500s......', 'css.jpg', NULL, NULL, NULL, NULL),
+(3, 'Autorius 3', '2021-06-22', 'Pazintis Su GIT', 'web programaviams', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\"s standard dummy text ever since the 1500s......', 'git.jpg', NULL, NULL, NULL, NULL),
+(4, 'Autorius 3', '2021-06-22', 'JavaScript Zaliems', 'web programaviams', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\"s standard dummy text ever since the 1500s......', 'js.jpg', NULL, NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `zp_kursas`
+--
+ALTER TABLE `zp_kursas`
+  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
